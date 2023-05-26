2c291d7b7e31d091ac3fffb62475f6b91be1edd8
ttomimeza <ttomasmeza@gmail.com>
2023-05-26 15:50:11 -0300

    cosas nuevas

--- catalog/urls.py
+++ catalog/urls.py
@@ -2,10 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index"),
-    path(r'^books/$', views.BookListView.as_view(), name='books'),
-]
-urlpatterns = [
     path(r'^$', views.index, name='index'),
     path(r'^books/$', views.BookListView.as_view(), name='books'),
     path(r'^book/(?P<pk>\d+)$', views.BookDetailView.as_view(), name='book-detail'),
